f9f243d14d6c4020e98c2a478579d8f2fdcaa1dd
KaalaAadmi <arnav264@gmail.com>
2022-10-16 05:05:43 +0100

    modification

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(`${api}`, dataRoute);
 
-app.use(express.static(path.join(__dirname, "/frontend")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
